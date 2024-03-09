99047c5e87088c1e8ac43755dc313b5bf5e1887a
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2024-03-09 03:51:35 -0500

    some fixes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -135,12 +135,10 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
 
-
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
-
     
     def test_delete_account(self):
         """It should Delete an Account"""
@@ -162,7 +160,6 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         updated_account = resp.get_json()
         self.assertEqual(updated_account["name"], "Something Known")
-
 
     def test_get_account_list(self):
         """It should Get a list of Accounts"""
